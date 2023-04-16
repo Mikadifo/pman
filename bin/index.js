5783f19f3e488cd4c7c16032c7d7af107721ebc6
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-04-16 12:35:03 -0400

    [fix] mangaing errros on cofig file, it has to be weell configured before asking the next question

--- bin/index.js
+++ bin/index.js
@@ -10,55 +10,69 @@ const configFilePath = path.join(os.homedir(), "/pmanrc.json");
 const configExists = fs.existsSync(configFilePath);
 
 const openProject = (projectPath) => {
-  spawn("open", ["-na", "iTerm", projectPath]);
-  console.log("Project opened.");
+  spawn("open", ["-a", "iTerm", projectPath]);
+  console.log("Project opened in a new tab. You can close this tab now.");
 };
 
 const showProjects = () => {
-  const configFile = JSON.parse(fs.readFileSync(configFilePath, "utf8"));
+  try {
+    const configFile = JSON.parse(fs.readFileSync(configFilePath, "utf8"));
 
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "directory",
-        message: "Choose a directory:",
-        choices: configFile.projectsDirs,
-      },
-      {
-        type: "list",
-        name: "project",
-        message: "Choose a project:",
-        choices: (answers) =>
-          fs
-            .readdirSync(answers.directory)
-            .filter((project) =>
-              fs.statSync(path.join(answers.directory, project)).isDirectory()
-            ),
-      },
-    ])
-    .then((answers) => {
-      openProject(path.join(answers.directory, answers.project));
-    });
+    if (configFile.projectsDirs.length <= 0) return createConfig();
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "directory",
+          message: "Choose a directory:",
+          choices: configFile.projectsDirs,
+        },
+        {
+          type: "list",
+          name: "project",
+          message: "Choose a project:",
+          choices: (answers) =>
+            fs
+              .readdirSync(answers.directory)
+              .filter((project) =>
+                fs.statSync(path.join(answers.directory, project)).isDirectory()
+              ),
+        },
+      ])
+      .then((answers) => {
+        openProject(path.join(answers.directory, answers.project));
+      });
+  } catch (error) {
+    return createConfig();
+  }
 };
 
 const createConfig = () => {
-  const configFile = JSON.parse(fs.readFileSync(configFilePath, "utf8"));
+  try {
+    const configFile = JSON.parse(fs.readFileSync(configFilePath, "utf8"));
 
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "newDir",
-        message: "Enter your projects directory:",
-        default: process.cwd(),
-      },
-    ])
-    .then((answers) => {
-      configFile.projectsDirs.push(path.normalize(answers.newDir));
-      fs.writeFileSync(configFilePath, JSON.stringify(configFile, null, 2));
-      showProjects();
-    });
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "newDir",
+          message: "Enter your projects directory:",
+          default: process.cwd(),
+        },
+      ])
+      .then((answers) => {
+        configFile.projectsDirs.push(path.normalize(answers.newDir));
+        fs.writeFileSync(configFilePath, JSON.stringify(configFile, null, 2));
+        showProjects();
+      });
+  } catch (error) {
+    fs.writeFileSync(
+      configFilePath,
+      JSON.stringify({ projectsDirs: [] }, null, 2)
+    );
+    return createConfig();
+  }
 };
 
 //Starts here
